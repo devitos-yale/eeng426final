92042c5162f10d02a128fd853ec5c035c146dbc6
devitos-yale <sophia.devito@yale.edu>
2022-12-20 16:23:32 -0500

    Added GND/Vdd m2 check

--- rectcheck.py
+++ rectcheck.py
@@ -1,16 +1,17 @@
 import sys
 
-ports = []
-m1 = []
-m2 = []
+ports = [] #array of possible connection points 
+m1 = [] #array of m1 wires
+m2 = [] #array of m2 wires
+m2Vdd = 0 #check if Vdd has m2 wires connected
+m2GND = 0 #check if GND has m2 wires connected
 
 port_distance = 3 #min distance between ports
 
-#sys.argv[1] should be a .rect file
+#check that the arguement is a .rect file
 if (len(sys.argv) != 2):
   print ("Usage python rectcheck.py <rect_file>")
   sys.exit (1)
-
 if (sys.argv[1][-5:] != ".rect"):
   print ("Expecting .rect extension")
   sys.exit (1)
@@ -22,8 +23,6 @@ old_rect_file = open(sys.argv[1], "r")
 #create new magic script for labels
 labels = open("labels.scr","w+")
 
-#get rid of exsisting labels for visibility
-
 #look at each line in input .rect file
 for line in old_rect_file:
 	a = line.split()
@@ -34,14 +33,26 @@ for line in old_rect_file:
 	#and Vdd/GND ports as m2 with Vdd and GND labels
 	if (a[0] == "outrect") or (a[0] == "inrect"): #input/output ports
 		ports.append(a)
-	if (a[1] == 'GND' or a[1] == 'Vdd') and (a[2] == 'm2'): #Vdd/GND ports
+	if (a[1] == 'GND') and (a[2] == 'm2'): #GND ports
 		ports.append(a)
+		m2GND = 1
+	if (a[1] == 'Vdd') and (a[2] == 'm2'): #Vdd ports
+		ports.append(a)
+		m2Vdd = 1
 
 	#create lists of m1 and m2 rectangles
 	if a[2] == "m1":
 		m1.append(a)
 	if a[2] == "m2":
 		m2.append(a)
+
+#check to see if GND/Vdd have m2 conenctions
+if m2GND == 0:
+	print ("GND is not connected to m2")
+	sys.exit (1)
+if m2Vdd == 0:
+	print ("Vdd is not connected to m2")
+	sys.exit (1)
 
 #check if the ports are < port_distance from each other
 for p in ports:
@@ -63,5 +74,6 @@ for p in ports:
 			#print ("MOVE RIGHT")
 			labels.write("move to %d %d \n" % (int(p[5]), int(p[6])))
 			labels.write("label MOVE_RIGHT \n")
+
 
 labels.close()
